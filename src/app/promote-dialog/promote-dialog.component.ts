d7e2fd5942bfea98c8a41cf9679d141d3da014ec
Nirbhay Singh <kumarnirbhay1986@gmail.com>
2021-12-31 18:03:44 -0600

    Fix

--- src/app/promote-dialog/promote-dialog.component.ts
+++ src/app/promote-dialog/promote-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { PromoteDialogSuccessComponenet } from '../promote-dialog-success/promote-dialog-success.component';
 
 @Component({
@@ -11,13 +12,14 @@ export class PromoteDialogComponenet {
   @Input() public promote: any;
   slectedEnv: any;
 
-  constructor(private dialog: MatDialog) {
-
+  constructor(private dialog: MatDialog, private _snackBar: MatSnackBar) {
+     
   }
   /** Open the promote dialog. */
   goTopromote() {
-    const modalRef = this.dialog.open(PromoteDialogSuccessComponenet, { panelClass: 'my-dialog', });
-    modalRef.componentInstance.promotesuccess = this.slectedEnv;
+    //const modalRef = this.dialog.open(PromoteDialogSuccessComponenet, { panelClass: 'my-dialog', });
+    //modalRef.componentInstance.promotesuccess = this.slectedEnv;
+    this._snackBar.open('Promotion successful. View the configuration in ' +this.slectedEnv,"Ok");
   }
   ngOnInit() {
     this.slectedEnv = localStorage.getItem("envType");
